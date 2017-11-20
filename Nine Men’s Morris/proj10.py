39e92c20cbdd19e3e8dcdd6cfaa10c311e412de9
Zhanhe Lyu <zhanhe0827@gmail.com>
2017-11-19 23:10:23 -0500

    init NMM

---
+++ Nine Men’s Morris/proj10.py
@@ -0,0 +1,158 @@
+
+
+import NMM #This is necessary for the project
+
+
+BANNER = """
+    __        _____ _   _ _   _ _____ ____  _ _ _ 
+    \ \      / /_ _| \ | | \ | | ____|  _ \| | | |
+     \ \ /\ / / | ||  \| |  \| |  _| | |_) | | | |
+      \ V  V /  | || |\  | |\  | |___|  _ <|_|_|_|
+       \_/\_/  |___|_| \_|_| \_|_____|_| \_(_|_|_)
+
+"""
+
+
+RULES = """
+  _   _ _              __  __            _       __  __                 _     
+ | \ | (_)_ __   ___  |  \/  | ___ _ __ ( )___  |  \/  | ___  _ __ _ __(_)___ 
+ |  \| | | '_ \ / _ \ | |\/| |/ _ \ '_ \|// __| | |\/| |/ _ \| '__| '__| / __|
+ | |\  | | | | |  __/ | |  | |  __/ | | | \__ \ | |  | | (_) | |  | |  | \__ \
+ |_| \_|_|_| |_|\___| |_|  |_|\___|_| |_| |___/ |_|  |_|\___/|_|  |_|  |_|___/
+                                                                                        
+    The game is played on a grid where each intersection is a "point" and
+    three points in a row is called a "mill". Each player has 9 pieces and
+    in Phase 1 the players take turns placing their pieces on the board to 
+    make mills. When a mill (or mills) is made one opponent's piece can be 
+    removed from play. In Phase 2 play continues by moving pieces to 
+    adjacent points. 
+    
+    The game is ends when a player (the loser) has less than three 
+    pieces on the board.
+
+"""
+
+
+MENU = """
+
+    Game commands (first character is a letter, second is a digit):
+    
+    xx        Place piece at point xx (only valid during Phase 1 of game)
+    xx yy     Move piece from point xx to point yy (only valid during Phase 2)
+    R         Restart the game
+    H         Display this menu of commands
+    Q         Quit the game
+    
+"""
+        
+def count_mills(board, player):
+    """
+        add your function header here.
+    """
+    pass  # stub; delete and replace it with your code
+            
+def place_piece_and_remove_opponents(board, player, destination):
+    """
+        add your function header here.
+    """
+    pass  # stub; delete and replace it with your code
+     
+def move_piece(board, player, origin, destination):
+    """
+        add your function header here.
+    """
+    pass  # stub; delete and replace it with your code
+    
+def points_not_in_mills(board, player):
+    """
+        add your function header here.
+    """
+    pass  # stub; delete and replace it with your code
+
+def placed(board,player):
+    """
+        add your function header here.
+    """
+    pass  # stub; delete and replace it with your code
+    
+def remove_piece(board, player):
+    """
+        add your function header here.
+    """
+    pass  # stub; delete and replace it with your code
+           
+def is_winner(board, player):
+    """
+        add your function header here.
+    """
+    pass  # stub; delete and replace it with your code
+   
+def get_other_player(player):
+    """
+    Get the other player.
+    """
+    return "X" if player == "O" else "O"
+    
+def main():
+    #Loop so that we can start over on reset
+    while True:
+        #Setup stuff.
+        print(RULES)
+        print(MENU)
+        board = NMM.Board()
+        print(board)
+        player = "X"
+        placed_count = 0 # total of pieces placed by "X" or "O", includes pieces placed and then removed by opponent
+        
+        # PHASE 1
+        print(player + "'s turn!")
+        #placed = 0
+        command = input("Place a piece at :> ").strip().lower()
+        print()
+        #Until someone quits or we place all 18 pieces...
+        while command != 'q' and placed_count != 18:
+            try:
+                
+                pass  # stub; delete and replace it with your code
+                
+            #Any RuntimeError you raise inside this try lands here
+            except RuntimeError as error_message:
+                print("{:s}\nTry again.".format(str(error_message)))
+            #Prompt again
+            print(board)
+            print(player + "'s turn!")
+            if placed_count < 18:
+                command = input("Place a piece at :> ").strip().lower()
+            else:
+                print("**** Begin Phase 2: Move pieces by specifying two points")
+                command = input("Move a piece (source,destination) :> ").strip().lower()
+            print()
+        
+        #Go back to top if reset
+        if command == 'r':
+            continue
+        # PHASE 2 of game
+        while command != 'q':
+            # commands should have two points
+            command = command.split()
+            try:
+                
+                pass  # stub; delete and replace it with your code
+                
+            #Any RuntimeError you raise inside this try lands here
+            except RuntimeError as error_message:
+                print("{:s}\nTry again.".format(str(error_message)))         
+            #Display and reprompt
+            print(board)
+            #display_board(board)
+            print(player + "'s turn!")
+            command = input("Move a piece (source,destination) :> ").strip().lower()
+            print()
+            
+        #If we ever quit we need to return
+        if command == 'q':
+            return
+
+            
+if __name__ == "__main__":
+    main()
